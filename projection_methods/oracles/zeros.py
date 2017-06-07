1767402339f2a65f774435d4a806752126ffb092
Akshay Agrawal <akshaykagrawal7@gmail.com>
2017-06-06 23:01:54 -0700

    return exactly the zero cone after querying; this change needs to be thought in greater detail

--- projection_methods/oracles/zeros.py
+++ projection_methods/oracles/zeros.py
@@ -1,6 +1,8 @@
 import numpy as np
+import scipy.sparse
 
 from projection_methods.oracles.convex_set import ConvexSet
+from projection_methods.projectables.hyperplane import Hyperplane
 
 
 class Zeros(ConvexSet):
@@ -13,6 +15,7 @@ class Zeros(ConvexSet):
         """
         constr = [x == 0]
         super(Zeros, self).__init__(x, constr)
+        self._return_zero_constraint = True
 
     def contains(self, x_0, atol=1e-6):
         return not np.any(np.absolute(x_0) > atol)
@@ -21,6 +24,15 @@ class Zeros(ConvexSet):
         return x_0 if self.contains(x_0) else np.zeros(x_0.shape)
 
     def query(self, x_0):
-        """NB: The  halfspace does not give information, so not returned."""
         x_star = self.project(x_0) 
-        return x_star, []
+        h = []
+        if self._return_zero_constraint:
+            A = scipy.sparse.eye(x_0.shape[0])
+            # This is an abuse of the word hyperplane; this function
+            # actually returns a set of hyperplanes that exactly identifies
+            # the zero set. If added to an outer approximation, the
+            # interpretation is that we are doing a presolve by forcing
+            # x to be 0.
+            h = [Hyperplane(self._x, A, 0)]
+            self._return_zero_constraint = False
+        return x_star, h
